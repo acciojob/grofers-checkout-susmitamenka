1f90a2d70c17b112124c519b49e1c7fb11888116
susmitamenka <susmitaprasad73@gmail.com>
2025-05-18 23:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll('.price');
+      let total = 0;
+
+      prices.forEach(price => {
+        total += parseFloat(price.textContent);
+      });
+
+      const table = document.getElementById('groceryTable');
+      const totalRow = document.createElement('tr');
+      totalRow.classList.add('total-row');
+      totalRow.innerHTML = `
+        <td>Total</td>
+        <td>${total.toFixed(2)}</td>
+      `;
+      table.appendChild(totalRow);
   
 };
 
